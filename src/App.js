e93e05b76499878d4e368e1fd9da7a7509053307
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-08 21:36:27 +0530

    class based components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-export default function App(){
-  return <p> Hello ! lets rewind and learn about class components !</p>
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props); //inherit from parent component ie react
+    this.state = { location: "Bengaluru" };
+    //need to manually bind the functiont to this - current comp instance 
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather() {
+    console.log("Loading weather details...");
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>🌞Classy weather⛈️</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather details</button>
+      </div>
+    );
+  }
 }
+
+export default App;
